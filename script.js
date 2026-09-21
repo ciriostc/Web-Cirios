c4b67e083a8ea02e65b84d2b8d533b1c28e3371a
ciriostc <cirios.tc@gmail.com>
2026-09-21 20:39:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// script.js
+// El sitio "Tus Cirios" no incluye funcionalidades de JavaScript por el momento.
+// Este archivo queda preparado para añadir interactividad en el futuro
+// (por ejemplo: validación de formularios, menú móvil, lightbox de galería, etc.)
